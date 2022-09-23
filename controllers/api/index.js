61c2d96c410e11d0ed46513a123dc0abf273ccd0
Gavin <gavinporlich@gmail.com>
2022-09-22 21:51:24 -0500

    Lint fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const chatRoomRoutes = require('./chatRoomRoutes')
+const chatRoomRoutes = require('./chatRoomRoutes');
 
 router.use('/users', userRoutes);
 router.use('/chatrooms', chatRoomRoutes);
