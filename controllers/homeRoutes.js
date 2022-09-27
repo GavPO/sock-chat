138d8e734dcb9b498e97b20dadda890b8b0c10c8
mvic13 <106556270+mvic13@users.noreply.github.com>
2022-09-26 20:44:41 -0500

    Updated route for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-// const { User } = require('../models');
-// const withAuth = require('../utils/auth');
+const { User, Chatroom, Message, Participant } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (_req, res) => {
   try {
@@ -15,10 +15,18 @@ router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     // CHANGE THIS TO WHEREVER YOUR PROJECT NEEDS TO GO
-    res.redirect('/logged_in_hompage');
+    res.redirect('/logged_in_homepage');
     return;
   }
   res.render('login');
+});
+
+router.get('/logged_in_homepage', async (req, res) => {
+  const allChatRooms = await Chatroom.findAll({ raw: true });
+  res.render('homepage', {
+    chatRoom: allChatRooms,
+    logged_in: req.session.logged_in,
+  });
 });
 
 module.exports = router;
